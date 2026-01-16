0c72abcf6a3530fdff33fa70aaee8a6fe9071f26
syaswanth456 <syaswanthkumar@hotmail.com>
2026-01-16 21:11:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,26 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+const subscriptions = {};
+
+app.post("/subscribe", (req, res) => {
+  const { code, subscription } = req.body;
+  subscriptions[code] = subscription;
+  res.json({ success: true });
+});
+
+app.post("/send-to-code", (req, res) => {
+  const { code, title, body } = req.body;
+  res.json({ success: true });
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
